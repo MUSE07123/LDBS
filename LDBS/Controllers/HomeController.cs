e72a1164f85a12895eb19b541df1861f527a9dbd
冠毅 吳 <cttlm364@gmail.com>
2022-09-10 17:56:29 +0800

    後端的一次修改

--- LDBS/Controllers/HomeController.cs
+++ LDBS/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LDBS.Models;
 
 namespace LDBS.Controllers
 {
@@ -25,6 +26,20 @@ namespace LDBS.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+
+
+        //test1
+        #region 變數宣告
+        private Stafflogin Stafflogin = new Stafflogin();
+        #endregion
+
+        public ActionResult Index2()
+        {
+            Stafflogin.StaffNumber_Check("1110831001");
+            //Stafflogin.ＵserPassword_check("1110831001", "00000001");
+            return Content("<html><body><h1> 某段訊息<h1><body><html>");
         }
     }
 }
