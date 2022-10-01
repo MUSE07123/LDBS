4692b0560b923780d5458c22381b75cd467c7af7
Fallmaple8125 <hsyuch8125@gmail.com>
2022-10-01 14:17:53 +0800

    新增Stafflogin頁面。新增Model：StaffloginCheck 。MemberController新增stafflogin帳密驗證。

--- LDBS/Controllers/MemberController.cs
+++ LDBS/Controllers/MemberController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LDBS.Models;
 
 namespace LDBS.Controllers
 {
@@ -21,6 +24,59 @@ namespace LDBS.Controllers
 
         public ActionResult Register()
         {
+            return View();
+        }
+
+        #region 變數宣告
+        private StaffloginCheck StaffloginCheck = new StaffloginCheck();
+        #endregion
+
+        public ActionResult Stafflogin()
+        {
+            //輸入的帳號密碼
+            string StaffAccount = Request.Form["StaffAccount"];
+            string StaffPassword = Request.Form["StaffPassword"];
+            //1110831001  00000001
+            try
+            {  //檢查輸入欄位是否有空值
+                if (StaffAccount == null || StaffPassword == null) { }
+                else
+                {
+                    if (StaffAccount == "" || StaffPassword == "")
+                    {
+                        ViewBag.msg = "請輸入帳號密碼！";
+                    }
+                    else
+                    {
+                        //檢查帳號是否正確
+                        if (StaffloginCheck.StaffNumber_Check(StaffAccount) == true)
+                        {
+                            ViewBag.Msg = "";
+                            //檢查密碼是否正確
+                            if (StaffloginCheck.UserPassword_check(StaffAccount, StaffPassword) == true)
+                            {
+                                ViewBag.Msg = "";
+                                ViewBag.Account = StaffAccount;
+                                return RedirectToAction("Index", "Staff");
+                            }
+                            else
+                            {
+                                ViewBag.Msg = "密碼錯誤！";
+                            }
+                        }
+                        else
+                        {
+                            //回傳帳號錯誤
+                            ViewBag.Msg = "此帳號不存在！";
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw (new Exception(ex.Message));
+            };
+
             return View();
         }
     }
---
+++ LDBS/Models/StaffloginCheck.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace LDBS.Models
+{
+    public class StaffloginCheck
+    {
+        private string strSQL = "";  //SQL指令
+        /// <summary>
+        /// 登入參數
+        /// </summary>
+        public class DoLoginIn
+        {
+            public string StaffAccount { get; set; }
+            public string StaffPassword { get; set; }
+        }
+        /// <summary>
+        /// 登入回傳
+        /// </summary>
+        /// 
+        public class DoLoginOut
+        {
+            public string ErrMsg { get; set; }
+            public string ResultMsg { get; set; }
+        }
+
+        /// <summary>
+        /// 檢查員工帳號
+        /// </summary>
+        public bool StaffNumber_Check(string StaffNumber)
+        {
+
+            bool boolretuen;
+
+            try
+            {
+                //1.新建連接對象
+                SqlConnection connection = new SqlConnection();
+                //2.連接字串
+                connection.ConnectionString = @"Data Source=.\MSSQLSERVER_2019;Initial Catalog=LDBS ;Integrated Security=SSPI;";
+                connection.Open(); //開資料庫
+                if (connection.State == ConnectionState.Open)
+                {
+
+                    // 檢查帳號是否存在
+                    strSQL = "";
+                    strSQL += "select StaffNumber from LDBS_StaffLogin where StaffNumber = @StaffNumber "; //SQL指令
+                    SqlCommand cmd = new SqlCommand();
+                    cmd.CommandText = strSQL;
+                    cmd.Connection = connection;
+
+                    // 使用參數化填值
+                    cmd.Parameters.AddWithValue("@StaffNumber", StaffNumber);
+
+                    // 執行資料庫查詢動作
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);      //查詢資料
+                    DataSet Ds = new DataSet();
+                    da.Fill(Ds);
+
+                    if (Ds.Tables[0].Rows.Count == 1)
+                    {
+                        boolretuen = true;        //確認有此筆資料
+                    }
+                    else
+                    {
+                        boolretuen = false;       //查無此資料
+                    }
+
+                    //關閉連線
+                    connection.Close();
+                    connection.Dispose();
+
+                    return boolretuen;
+                }
+                else
+                {
+                    return false;       //失敗
+                }
+            }
+            catch (Exception ex)
+            {
+                throw (new Exception(ex.Message));
+            }
+        }
+        /// <summary>
+        /// 檢查員工密碼(如果正確則跳轉) //未測試
+        /// </summary>
+        public bool UserPassword_check(string StaffNumber, string Password)
+        {
+
+            bool boolretuen;
+            try
+            {
+                //1.新建連接對象
+                SqlConnection connection = new SqlConnection();
+                //2.連接字串
+                connection.ConnectionString = @"Data Source=.\MSSQLSERVER_2019;Initial Catalog=LDBS ;Integrated Security=SSPI;";
+                connection.Open(); //開資料庫
+                strSQL = "";
+                strSQL += "select StaffNumber,UserPassword from LDBS_StaffLogin where StaffNumber = @StaffNumber and UserPassword  = @Password";
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandText = strSQL;
+                cmd.Connection = connection;
+                // 使用參數化填值
+                cmd.Parameters.AddWithValue("@StaffNumber", StaffNumber);
+                cmd.Parameters.AddWithValue("@Password", Password);
+
+                // 執行資料庫查詢動作
+                SqlDataAdapter da = new SqlDataAdapter(cmd);      //查詢資料
+                DataSet Ds = new DataSet();
+                da.Fill(Ds);
+
+                if (Ds.Tables[0].Rows.Count == 1)
+                {
+                    boolretuen = true;          //確認有此筆資料
+                }
+                else
+                {
+                    boolretuen = false;        //查無此資料
+                }
+                return boolretuen;
+
+            }
+            catch (Exception ex)
+            {
+                throw (new Exception(ex.Message));
+            }
+
+
+        }
+    }
+}
